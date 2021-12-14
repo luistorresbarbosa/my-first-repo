b678c296305655b73b6bc22546ddecfa39ab6ea4
luistorresbarbosa <85165499+luistorresbarbosa@users.noreply.github.com>
2021-12-14 14:25:15 +0000

    Update install_docker_centos.sh

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -10,5 +10,5 @@ sudo yum install -y yum-utils
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io --nobest
 sudo systemctl start docker
