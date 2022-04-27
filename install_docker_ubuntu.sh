deb9d0211fb76d32e9048d2713cf3e00e58a47c5
luistorresbarbosa <85165499+luistorresbarbosa@users.noreply.github.com>
2022-04-27 14:45:56 +0100

    Update install_docker_ubuntu.sh

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -2,4 +2,6 @@ sudo apt-get update -y
 sudo apt-get install -y device-mapper-persistent-data lvm2
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get install docker.io
+sudo systemctl enable docker
 sudo systemctl start docker
+sudo docker --version
