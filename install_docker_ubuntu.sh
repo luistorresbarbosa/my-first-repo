3ce59984c0c66760321e7aa6d6f0bfc3654606bc
luistorresbarbosa <85165499+luistorresbarbosa@users.noreply.github.com>
2022-04-27 14:37:24 +0100

    Update install_docker_ubuntu.sh

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,1 +1,5 @@
-
+sudo apt-get update -y
+sudo apt-get install -y yum-utils device-mapper-persistent-data lvm2
+sudo apt-get remove docker docker-engine docker.io containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
