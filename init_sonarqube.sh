83b8d2c63d06af056e5f7f0e879a0edc18154dda
luistorresbarbosa <85165499+luistorresbarbosa@users.noreply.github.com>
2023-10-16 09:51:09 +0100

    Update init_sonarqube.sh

--- init_sonarqube.sh
+++ init_sonarqube.sh
@@ -15,3 +15,5 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod 666 /var/run/docker.sock
+
